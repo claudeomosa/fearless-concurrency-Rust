dd09ee2b0392c2fce7d4967f042db008f8243516
claudeomosa <claudeomosa@gmail.com>
2022-12-22 08:08:01 +0300

    refactor: reduce verbose code by having status code and filename in a tuple instead of if-else blocks success, single threaded web server created

--- src/main.rs
+++ src/main.rs
@@ -21,25 +21,21 @@ fn handle_connection(mut stream: TcpStream) {
     //this lie gets us the first line of the http response (the request line) rather than the whole
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
         // this handles request from the URL with '/' URI
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
-
-    } else {
+        ("HTTP/1.1 200 OK", "hello.html")
+    }else {
         // this handles request from any other URI
-        let status_line = "HTTP/1.1 404 PageNotFound";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
+        ("HTTP/1.1 404 PageNotFound", "404.html")
+    };
 
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-    }
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+
+
     /*
     //this fetches the http_request details
     let http_request: Vec<_> = buf_reader
